6659db521caac14421a83a3127da95cb0d51fd46
Mostapha Alahmair <153405663+Mostapha45@users.noreply.github.com>
2024-09-18 16:21:34 -0600

    Updated home screen

--- app/page.js
+++ app/page.js
@@ -3,8 +3,18 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Go to Week 2 Page</Link>
+      <h1 className="text-xl text-center font-bold mb-4">CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="/week-2" className="text-center block text-blue-500 hover:underline mb-2">
+        Go to Week 2 Page
+      </Link>
+      <Link href="/week-3" className="text-center block text-blue-500 hover:underline mb-2">
+        Go to Week 3 Page
+      </Link>
+      <Link href="/week-4" className="text-center block text-blue-500 hover:underline">
+        Go to Week 4 Page
+      </Link>
     </div>
   );
 }
+
+
