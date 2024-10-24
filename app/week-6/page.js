80cb58488ae5fd6c63dd6f3d021917861333eecc
Mostapha45 <153405663+Mostapha45@users.noreply.github.com>
2024-10-23 21:47:47 -0600

    updated bg color of week-6

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -3,7 +3,7 @@ import ItemList from './item-list';
 
 const Page = () => {
     return (
-        <div>
+        <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-600">
             <h1 className='text-xl font-bold'>Week 6 Assignment</h1>
             <h1 className='text-3xl font-bold'>Shopping List</h1>
             <ItemList />
